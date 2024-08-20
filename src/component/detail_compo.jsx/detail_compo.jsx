d44e4127a128c514ceae051ead4cc5f61b0dee7a
SaraGamal2000 <dr.sara.masoud1@gmail.com>
2024-08-20 21:17:04 +0300

    some edits3

--- src/component/detail_compo.jsx/detail_compo.jsx
+++ src/component/detail_compo.jsx/detail_compo.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
 import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function Detail_compo({ movie, Recommend = [] }) {
   const {
@@ -51,7 +52,7 @@ function Detail_compo({ movie, Recommend = [] }) {
               if (index < fullStars) {
                 return (
                   <FontAwesomeIcon
-                    style={{ color: "black" , fontSize: "25px"}}
+                    style={{ color: "black", fontSize: "25px" }}
                     key={index}
                     icon={faStar}
                     className="star filled"
@@ -60,7 +61,7 @@ function Detail_compo({ movie, Recommend = [] }) {
               } else if (index === fullStars && hasHalfStar) {
                 return (
                   <FontAwesomeIcon
-                    style={{ color: "black" , fontSize: "25px"}}
+                    style={{ color: "black", fontSize: "25px" }}
                     key={index}
                     icon={faStarHalfAlt}
                     className="star half-filled"
@@ -77,7 +78,9 @@ function Detail_compo({ movie, Recommend = [] }) {
                 );
               }
             })}
-            <span style={{fontSize:"25px",color:"gray",margin:"20px"}}>{vote_count}</span>
+            <span style={{ fontSize: "25px", color: "gray", margin: "20px" }}>
+              {vote_count}
+            </span>
           </div>
 
           <p style={{ width: "838px", height: "145px", fontSize: "24px" }}>
@@ -139,23 +142,33 @@ function Detail_compo({ movie, Recommend = [] }) {
         <Row style={{ display: "flex", flexWrap: "wrap" }}>
           {Recommend && Recommend.length > 0 ? (
             Recommend.map((rec) => (
-              <Col key={rec.id} xs={6} md={4} lg={3}>
-                <div className="recommendation-item">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${rec.poster_path}`}
-                    alt={rec.title}
-                    style={{
-                      width: "200px",
-                      marginLeft: "20px",
-                      height: "300px",
-                      borderRadius: "15PX",
-                    }}
-                  />
-                  <h2 style={{ marginLeft: "20px", width: "200px" }}>
-                    {rec.title}
-                  </h2>
-                  <p style={{ marginLeft: " 20px", color: "gray" }}>
-                    {rec.release_date}
+              <Col key={rec.id}>
+                <div
+                  className="movie-card"
+                  style={{
+                    width: "200px",
+                    marginLeft: "20px",
+                    height: "400px",
+                    marginBottom: "20px",
+                  }}
+                >
+                  <Link to={`/details/${rec.id}`}>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${rec.poster_path}`}
+                      alt={rec.title}
+                      className="movie-poster"
+                    />
+                  </Link>
+                  <h3>{rec.title}</h3>
+
+                  <p>{rec.release_date}</p>
+                  <p className="ico">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
+                    </svg>
                   </p>
                 </div>
               </Col>
